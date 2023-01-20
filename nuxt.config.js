81410d3c2370d525ce2a930e1fbaf2f0fe744493
javy3r18 <javy18ms@gmail.com>
2023-01-20 13:49:44 -0800

    create user done

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 export default {
+  ssr: false,
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'firebase',
@@ -28,13 +29,34 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/bootstrap
+    [
+      '@nuxtjs/firebase',
+      {
+        config: {
+          apiKey: process.env.FIREBASE_API_KEY,
+          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER,
+          appId: process.env.FIREBASE_APP_ID
+        },
+        services: {
+          auth: true // Just as example. Can be any other service.
+        }
+      }
+    ],
     'bootstrap-vue/nuxt',
+  ],
+
+  buildModules: [
+    // Simple usage
+    '@nuxtjs/dotenv',
+
+    // With options
+    ['@nuxtjs/dotenv', { /* module options */ }]
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,61 @@
 <template>
-  <Tutorial/>
+  <div>
+    <b-container>
+      <b-form @submit.prevent="onSubmit">
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          placeholder="Enter email"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Password:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.password"
+          type="password"
+          placeholder="Enter password"
+          required
+        ></b-form-input>
+      </b-form-group>
+      <b-button type="submit" variant="primary">Submit</b-button>
+    </b-form>
+    </b-container>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  data(){
+    return{
+      form: {
+        email: null,
+        password: null
+      }
+    }
+  },
+  mounted(){
+    console.log(this.$fire.auth);
+  },
+
+  methods: {
+    async onSubmit(){
+      try {
+        await this.$fire.auth.createUserWithEmailAndPassword(this.form.email, this.form.password)
+      }
+      catch (e) {
+        console.error(e)
+      }
+    }
+  }
+
 }
 </script>
